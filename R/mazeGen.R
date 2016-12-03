510ba69768d7adcf746f35e06fc31ef0bde1425c
bsl28@cam.ac.uk <bsl28@cam.ac.uk>
2016-12-03 13:36:12 +0000

    description package

---
+++ R/mazeGen.R
@@ -0,0 +1,11 @@
+#' mazeGen: A package for generating number series items.
+#'
+#' The mazeGen package provides 14 item models for generating number series items
+#'
+#' Still need to make changes here.
+#' @section Item model 1:
+#' This number series counts the alphabets representation.\cr
+#' \code{\link{nmOne}}
+#'
+#' @name numGen
+NULL
