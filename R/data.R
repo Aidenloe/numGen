80db30b68a66dcdbf9208409aee921fe3ba586e6
bsl28@cam.ac.uk <bsl28@cam.ac.uk>
2016-12-05 11:33:13 +0000

    grid

--- R/data.R
+++ R/data.R
@@ -1201,3 +1201,95 @@
 #' }
 #'
 "gridFifteenRight"
+
+#' Grid Sixteen Up
+#'
+#' This returns a sixteen grid upwards maze. These are standardized coordinates.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name gridSixteenUp
+#' @usage data(gridSixteenUp)
+#' @format A data frame with 2 columns
+#'\describe{
+#'   \item{start}{Coordinates of Start Node.}
+#'   \item{end}{Coordinates End Node.}
+#' }
+#' @examples \dontrun{
+#'
+#' # Returns a Grid with rank = 16
+#' data(gridSixteenUp)
+#' coordinates <- gridSixteenUp
+#'
+#' }
+#'
+"gridSixteenUp"
+
+#' Grid Sixteen Down
+#'
+#' This returns a sixteen grid downards maze. These are standardized coordinates.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name gridSixteenDown
+#' @usage data(gridSixteenDown)
+#' @format A data frame with 2 columns
+#'\describe{
+#'   \item{start}{Coordinates of Start Node.}
+#'   \item{end}{Coordinates End Node.}
+#' }
+#' @examples \dontrun{
+#'
+#' # Returns a Grid with rank = 16
+#' data(gridSixteenDown)
+#' coordinates <- gridSixteenDown
+#'
+#' }
+#'
+"gridSixteenDown"
+
+#' Grid Sixteen Left
+#'
+#' This returns a sixteen grid left maze. These are standardized coordinates.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name gridSixteenLeft
+#' @usage data(gridSixteenLeft)
+#' @format A data frame with 2 columns
+#'\describe{
+#'   \item{start}{Coordinates of Start Node.}
+#'   \item{end}{Coordinates End Node.}
+#' }
+#' @examples \dontrun{
+#'
+#' # Returns a Grid with rank = 16
+#' data(gridSixteenLeft)
+#' coordinates <- gridSixteenLeft
+#'
+#' }
+#'
+"gridSixteenLeft"
+
+#' Grid Sixteen Right
+#'
+#' This returns a sixteen grid right maze. These are standardized coordinates.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name gridSixteenRight
+#' @usage data(gridSixteenRight)
+#' @format A data frame with 2 columns
+#'\describe{
+#'   \item{start}{Coordinates of Start Node.}
+#'   \item{end}{Coordinates End Node.}
+#' }
+#' @examples \dontrun{
+#'
+#' # Returns a Grid with rank = 16
+#' data(gridSixteenRight)
+#' coordinates <- gridSixteenRight
+#'
+#' }
+#'
+"gridSixteenRight"
